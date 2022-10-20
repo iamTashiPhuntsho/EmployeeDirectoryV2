75b3e3d9c5e685098839277f9f087f6190fd635b
Sherab Wangchuk <LT2022060102@bank.bnb>
2022-10-20 16:32:28 +0600

    new

--- resources/views/backend/edit.blade.php
+++ resources/views/backend/edit.blade.php
@@ -72,7 +72,7 @@
                             </div>
                             <div>
                                 <label for="present_address" class="text-xs font-semibold text-blue-800">Present Address</label>
-                                <input type="text" class="w-full rounded-sm" placeholder="Present Address" name="present_address" id="present_address" required="required" value="{{ $employee->present_address }}">
+                                <input type="text" class="w-full rounded-sm" placeholder="Present Address" name="present_address" id="present_address" value="{{ $employee->present_address }}">
                             </div>
                             <div>
                                 <label for="status" class="text-xs font-semibold text-blue-800">Status</label>
