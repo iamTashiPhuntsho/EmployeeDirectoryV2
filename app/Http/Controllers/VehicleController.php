1f85f2c5da29c04ae95d88af4d78efe83984bdae
Sherab Wangchuk <LT2022060102@bank.bnb>
2022-10-20 16:42:51 +0600

    new

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -17,13 +17,13 @@ class VehicleController extends Controller
         $ia = Employee::with('contact')->where('employee_id','2001060108')->first();
         $rd = Employee::with('contact')->where('employee_id','2001040905')->first();
         $smd = Employee::with('contact')->where('employee_id','1995021302')->first();
-        $cos = Employee::with('contact')->where('employee_id','1995021302')->first();
+        $cos = Employee::with('contact')->where('employee_id','2003100113')->first();
         $cex = Employee::with('contact')->where('employee_id','2008080112')->first();
         $bo = Employee::with('contact')->where('employee_id','2008080112')->first();
         $fin = Employee::with('contact')->where('employee_id','2003100113')->first();
         $hra = Employee::with('contact')->where('employee_id','1993120702')->first();
         $dtd = Employee::with('contact')->where('employee_id','2003072507')->first();
-        $cse = Employee::with('contact')->where('employee_id','2003100113')->first();
+        $cse = Employee::with('contact')->where('employee_id','1997070109')->first();
         $ito = Employee::with('contact')->where('employee_id','2005020103')->first();
         $bro = Employee::with('contact')->where('employee_id','1996040101')->first();
         $rem = Employee::with('contact')->where('employee_id','1996121104')->first();
