359f38d67024b34f110740a95a91082eec22ad12
Sherab Wangchuk <LT2022060102@bank.bnb>
2022-11-14 14:57:25 +0600

    newew

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -102,8 +102,8 @@
                            animate(text2, 100, 11, 3000);
                            animate(text3, 100, 26, 3000);
                            animate(text4, 100, 12, 3000);
-                           animate(text5, 100, 300, 3000);
-                           animate(text6, 100, 229, 3000);
+                           animate(text5, 100, 235, 3000);
+                           animate(text6, 100, 304, 3000);
                            }
                         </script>
                      </body>
