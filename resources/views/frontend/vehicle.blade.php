e15997980efeefee5f7b1b377f6c1aeb22949169
Sherab Wangchuk <LT2022060102@bank.bnb>
2023-03-01 11:01:30 +0600

    newewe

--- resources/views/frontend/vehicle.blade.php
+++ resources/views/frontend/vehicle.blade.php
@@ -172,7 +172,7 @@
                   <div class="custom-row">
                      <div class="col-4 px-1 text-center">
                         <div class="container-box">
-                        <div class="main-box cs" data-bs-toggle="modal" data-bs-target="#myModal" data-info="{{$cos}}" style = "position:relative; bottom:25px;">
+                        <div class="main-box cs" data-bs-toggle="modal" data-bs-target="#myModal" data-info="{{$fin}}" style = "position:relative; bottom:25px;">
                            <img src='{{asset("storage/employee_images/$fin->image")}}' alt="" class="img">
                               <div class="description">
                                  Finance
